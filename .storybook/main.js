c1cd6846ca58a5c9d6344c04adcd52e185d93cb6
Matthew Hampton <matthew.j.hampton@gmail.com>
2025-03-19 10:20:46 +0200

    ran npx sv@0.6.18 create and chose the storybook option

---
+++ .storybook/main.js
@@ -0,0 +1,25 @@
+
+
+/** @type { import('@storybook/sveltekit').StorybookConfig } */
+const config = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|ts|svelte)"
+  ],
+  "addons": [
+    {
+      "name": "@storybook/addon-essentials",
+      "options": {
+        "docs": false
+      }
+    },
+    "@storybook/addon-svelte-csf",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions"
+  ],
+  "framework": {
+    "name": "@storybook/sveltekit",
+    "options": {}
+  }
+};
+export default config;
